55c4d7af43cbc23ca2f5a016d716f42711110b3f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-10-26 11:39:13 -0400

    Estructura

--- estructurabd/seg_permiso.php
+++ estructurabd/seg_permiso.php
@@ -2,7 +2,7 @@
 include_once(dirname(__FILE__).DIRECTORY_SEPARATOR."../class/bd.php");
 class seg_permiso extends bd{
 	var $campos_tabla=array(
-		"cod_permiso"=>array("Tipo"=>"varchar","Tamano"=>"3","NoNulo"=>true,"Primaria"=>true,"AI"=>false),
+		"cod_permiso"=>array("Tipo"=>"varchar","Tamano"=>"6","NoNulo"=>true,"Primaria"=>true,"AI"=>false),
 		"cod_sistema"=>array("Tipo"=>"varchar","Tamano"=>"5"),
 		"descripcion"=>array("Tipo"=>"varchar","Tamano"=>"60"),
 	);	
