0ee19b7dce8c33c862f4b9c37fa31213935cd2b0
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-10-26 18:46:58 -0400

    Pruebas

---
+++ administrar/pruebas/actualizar.php
@@ -0,0 +1,13 @@
+<?php
+include_once("../../estructurabd/rec_tipo_prueba.php");
+extract($_POST);
+$valores=array(
+				"cod_tipo"=>"'$cod_tipo'",
+				"descripcion"=>"'$descripcion'",
+
+				);
+$rec_tipo_prueba=new rec_tipo_prueba;
+$rec_tipo_prueba->actualizarRegistro($valores,"cod_tipo='$cod_tipo'");
+
+header("Location:../?c=Tipo de Pruebas");
+?>
