fb4f264bf820ba8d6932ca9718d7cb06bf7a39c2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-10-26 11:34:52 -0400

    Corregido Seg_Sistema

--- estructurabd/seg_sistema.php
+++ estructurabd/seg_sistema.php
@@ -2,7 +2,7 @@
 include_once(dirname(__FILE__).DIRECTORY_SEPARATOR."../class/bd.php");
 class seg_sistema extends bd{
 	var $campos_tabla=array(
-		"cod_sistema"=>array("Tipo"=>"varchar","Tamano"=>"3","NoNulo"=>true,"Primaria"=>false,"AI"=>false),
+		"cod_sistema"=>array("Tipo"=>"varchar","Tamano"=>"3","NoNulo"=>true,"Primaria"=>true,"AI"=>false),
 		"descripcion"=>array("Tipo"=>"varchar","Tamano"=>"60"),
 		"img_01"  =>array("Tipo"=>"varchar","Tamano"=>"150"),
 		"img_02"  =>array("Tipo"=>"varchar","Tamano"=>"150"),
