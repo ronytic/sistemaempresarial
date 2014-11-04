1eaa77d3d931b6d40de9bd09b3719fef0ae56208
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-11-04 10:54:13 -0400

    Login

--- login/index.php
+++ login/index.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 //print_r($_SESSION);
+include_once("../estructurabd/seg_empresa.php");
+$seg_empresa=new seg_empresa;
+$seg_e=$seg_empresa->mostrarTodoRegistro("",0,"descripcion");
 ?>
 <!doctype html>
 <html>
@@ -33,8 +36,19 @@ session_start();
 <form action="login.php" method="post">
 	<input type="hidden" name="u" value="<?php echo $_GET['u'];?>">
 	<table>
+    	<tr><td>Empresa</td><td><select name="cod_empresa" autofocus required>
+            	<option value="">Seleccionar</option>
+            	<?php foreach($seg_e as $se){
+				?>
+                <option value="<?php echo $se['cod_empresa']?>"><?php echo $se['cod_empresa']?> - <?php echo $se['descripcion']?></option>
+                <?php	
+				}?>
+            </select>
+            
+            
+            </td></tr>
     	<tr><td>Usuario</td><td><input type="text" name="usuario"></td></tr>
-        <tr><td>Contraseña</td><td><input type="pass" name="contrasena"></td></tr>
+        <tr><td>Contraseña</td><td><input type="password" name="contrasena"></td></tr>
         <tr><td></td><td><input type="submit" name="" value="Ingresar"></td></tr>
     </table>
 </form>
