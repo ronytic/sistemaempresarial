186999bd36aec0d6b1ffd3951e7441e54ea92f97
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-24 19:43:08 -0400

    Pruebas Logib

---
+++ pruebas/login/index.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+//print_r($_SESSION);
+$folder="../";
+include_once("../estructurabd/seg_empresa.php");
+$seg_empresa=new seg_empresa;
+$seg_e=$seg_empresa->mostrarTodoRegistro("",0,"descripcion");
+?>
+<!DOCTYPE html>
+<html dir="ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso</title>
+   
+
+    <link href="css/estilo.css" rel="stylesheet" type="text/css">
+   <style type="text/css">
+   	select{
+		padding:7px;
+		margin-right:26px;
+		border:0px solid #FFFFFF;
+		background:none;
+		width:250px;
+		outline:none;
+	}
+   </style>
+</head>
+<body><div id="preload_images"></div>
+
+<img class="background-image" src="imagenes/fondo.png">
+
+<div id="login-wrapper" style="opacity: 1; visibility: visible;">
+    <div id="notify">
+        
+
+        <div id="login-status" class="error-notice" style="visibility: hidden">
+            <span class="login-status-icon"></span>
+            
+        </div>
+    </div>
+
+    
+
+    <div id="content-container">
+        <div id="login-container">
+            <div id="login-sub-container">
+                <div id="login-sub-header">
+<div class="titulo"><img src="imagenes/titulo.png" border="0"></div>
+                </div>
+                <div id="login-sub">
+                    <div id="forms">
+             
+                        <form id="login_form" action="login.php" method="post" target="_top">
+                        	<div class="input-req-login"><label for="empresa">Empresa</label></div>
+                            <div class="input-field-login icon username-container">
+                                <select name="cod_empresa" autofocus required>
+                                    <option value="">Seleccionar</option>
+                                    <?php foreach($seg_e as $se){
+                                    ?>
+                                    <option value="<?php echo $se['cod_empresa']?>"><?php echo $se['cod_empresa']?> - <?php echo $se['descripcion']?></option>
+                                    <?php	
+                                    }?>
+                                </select>
+                            </div>
+                            
+                            
+                            <div class="input-req-login"><label for="user">Nombre de Usuario</label></div>
+                            <div class="input-field-login icon username-container">
+                                <input name="usuario" id="user" autofocus value="" placeholder="Introduzca su nombre de usuario." class="std_textbox" type="text" tabindex="1" required>
+                            </div>
+                            <div style="margin-top:30px;" class="input-req-login"><label for="pass">Contraseña</label></div>
+                            <div class="input-field-login icon password-container">
+                                <input name="contrasena" id="pass" placeholder="Ingrese su contraseña de la cuenta." class="std_textbox" type="password" tabindex="2" required>
+                            </div>
+                            <div class="controls">
+                                <div class="login-btn">
+                                    <button name="login" type="submit" id="login_submit" tabindex="3">Acceder</button>
+                                  <p>&nbsp;</p>
+                                </div>
+                                          
+                                   
+                                                            </div>
+                            <div class="clear" id="push"></div>
+                        </form>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="locale-footer" style="display: block;">
+            <div class="locale-container">
+                <noscript>
+                    
+                </noscript>
+                
+            </div>
+        </div>
+    </div>
+
+</div>
+
+    <div class="copyright"></div>
+
+</body></html>
---
+++ pruebas/login/login.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+@set_magic_quotes_runtime(0);
+//header("Content-Type: text/html;charset=utf-8");
+if(!empty($_POST)){
+	
+/*	require_once('php-local-browscap.php');
+	$navegador=get_browser_local(null,true,"lite_php_browscap.ini",true);
+	print_r($navegador);*/
+
+	include_once("../configuracion.php");
+	include_once("../estructurabd/seg_usuario.php");
+	
+	//include_once("../class/logusuario.php");
+	$seg_usuario=new seg_usuario;
+	//$logusuario=new logusuario;
+	
+	$url=$_POST['u'];
+	if(empty($directory) || $directory==""){
+		$u=$url;	
+		$direccion="..".$u;
+	}else{
+		$u=explode($directory,$_POST['u']);
+		$direccion="../".$u[1];
+	}
+	
+	
+	if(isset($_POST['usuario'],$_POST['contrasena']) && $_POST['usuario']!="" && $_POST['contrasena']!=""){
+		
+		$usuario=($_POST['usuario']);
+		$pass=$_POST['contrasena'];
+		$cod_empresa=$_POST['cod_empresa'];
+		
+//		$usuario=str_replace("ñ","n",$usuario);
+		$usuarioAl=str_replace(array("ñ","Ñ"),array("n","N"),$usuario);
+		$usuarioAl=strtolower($usuarioAl);
+	//	echo $usuarioAl;
+	
+		$agente=$_SERVER['HTTP_USER_AGENT'];
+		$ip=$_SERVER['REMOTE_ADDR'];
+		$lenguaje=$_SERVER['HTTP_ACCEPT_LANGUAGE'];
+		$referencia= $_SERVER['HTTP_REFERER'];
+		$fecha=date("Y-m-d");
+		$hora=date("H:i:s");
+		//$reg=$alumno->loginAlumno($usuario,$pass);
+		
+		if($usuario!=""){
+			//Administrador 1 2 3 4
+			$reg=$seg_usuario->loginUsuarios($usuario,$pass,$cod_empresa);
+			$reg=array_shift($reg);
+			$sw=1;
+		}else{
+			//echo $sql;
+			header("Location:./?u=".$url.'&error=1');		
+		}
+		//echo $Nivel;
+		/**/
+		
+		//$res=mysql_query($sql);
+		//@$reg=mysql_fetch_array($res);
+		$cod_sistema=$reg['cod_sistema'];
+		$cod_empresa=$reg['cod_empresa'];
+		
+		if($sw){
+			//$Nivel=$reg['Nivel'];
+		}
+		
+		if($reg['can']==1){
+			/*$valuesLog=array(
+				"CodUsuario"=>$codUsuario,
+				"Nivel"=>$Nivel,
+				"Url"=>"'$url'",
+				"FechaLog"=>"'$fecha'",
+				"HoraLog"=>"'$hora'",
+				"Agente"=>"'$agente'",
+				"Ip"=>"'$ip'",
+				"Referencia"=>"'$referencia'",
+				"Lenguaje"=>"'$lenguaje'"
+			);
+			$logusuario->insertarRegistro($valuesLog,0);
+			//mysql_query("INSERT INTO logusuarios VALUES(NULL,$codUsuario,$Nivel,'$url','$fecha','$hora','$agente','$ip','$referencia','$lenguaje')");
+			*/
+			$_SESSION['cod_usuario']=$reg['cod_usuario'];;
+			$_SESSION['login']=$reg['login'];
+			$_SESSION['nombre']=$reg['nombre'];
+			$_SESSION['paterno']=$reg['paterno'];
+			$_SESSION['materno']=$reg['materno'];
+			$_SESSION['cod_rol']=$reg['cod_rol'];
+			
+			$_SESSION['cod_sistema']=$cod_sistema;
+			$_SESSION['cod_empresa']=$cod_empresa;
+			$_SESSION['logueado']=1;
+			$_SESSION['hora_ingreso']=date("H:i:s");
+			$_SESSION['fecha_ingreso']=date("Y-m-d");
+			/*$_SESSION['Idioma']=$reg['Idioma'];
+			echo $logusuario->optimizarTablas();*/
+			//echo $direccion;
+			header("Location:".$direccion);
+		}
+		else{
+			header("Location:./?u=".$url.'&error=1');
+		}
+	}else{
+		header("Location:./?u=".$url.'&incompleto=1');	
+	}
+}
+?>
