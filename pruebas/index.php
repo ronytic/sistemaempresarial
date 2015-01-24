f12e9a14c81d553eb70f12850cfd844cdba9820e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-24 19:43:30 -0400

    Pruebas Estructuras

---
+++ pruebas/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once("login/check.php");
+header("location:administrarreclutamiento/?c=reclutamiento");
+include_once("cabecerahtml.php");
+include_once("cabecera.php");
+?>
+
+<?php
+include_once("pie.php");
+?>
