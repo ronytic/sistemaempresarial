b66fff73548e68ea427690301642a91ad0f1da9a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-01-24 19:41:46 -0400

    Corregido Funciones

--- funciones/funciones.php
+++ funciones/funciones.php
@@ -9,4 +9,33 @@ function listarDirectorio($dir){
 	}
 	return $archivos;
 }
+function capitalizar($texto){
+	return ucwords($texto);
+}
+function mayuscula($texto){
+	return mb_strtoupper($texto,"utf8");
+}
+function minuscula($texto){
+	return mb_strtolower($texto,"utf8");
+}
+function precio($Cantidad,$decimal=2){
+	return number_format($Cantidad,$decimal);
+}
+function fecha2Str($fecha="",$t=1){
+	$fecha=$fecha==""?date("Y-m-d"):$fecha;
+	if(date("d-m-Y",strtotime($fecha))=='31-12-1969'||date("Y-m-d",strtotime($fecha))=='1969-12-31'){
+	return $fecha;}
+	if(!empty($fecha) && $fecha!="0000-00-00"){
+		if($t==1){
+			return date("d-m-Y",strtotime($fecha)."+5day");	
+		}else{
+			return date("Y-m-d",strtotime($fecha));	
+		}
+	}else{
+		if($t=1 && $fecha=="0000-00-00")
+		return "00-00-0000";
+		else
+		return $fecha;
+	}
+}
 ?>
