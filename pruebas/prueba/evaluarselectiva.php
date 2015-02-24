cd76915c2ae606a166e9633e95340e137c7b69db
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-02-24 17:02:10 -0400

    Corregido Selectiva

---
+++ pruebas/prueba/evaluarselectiva.php
@@ -0,0 +1,147 @@
+<?php
+include_once("../login/check.php");
+$folder="../../";
+include_once("../../estructurabd/rec_banco_candidato.php");
+$rec_banco_candidato=new rec_banco_candidato;
+
+include_once("../../estructurabd/rec_candidato.php");
+$rec_candidato=new rec_candidato;
+
+include_once("../../estructurabd/rec_banco_preguntas.php");
+$rec_banco_preguntas=new rec_banco_preguntas;
+
+include_once("../../estructurabd/rec_prueba.php");
+$rec_prueba=new rec_prueba;
+
+$cedula=$_SESSION['cedula'];
+$cod_planta=$_SESSION['cod_planta'];
+$cod_recluta=$_SESSION['cod_recluta'];
+$pruebas=$_SESSION['pruebas'];
+$cod_empresa=$_SESSION['cod_empresa'];
+
+
+$pruebaactual=array_shift($pruebas);
+$orden_prueba=array_shift(array_keys($pruebaactual));
+
+$cod_prueba=$_POST['cod_prueba'];
+$cod_banco=$_POST['cod_banco'];
+$r=$_POST['r'];
+$rec_c=$rec_candidato->mostrarTodoRegistro("cedula='$cedula'",0);
+$rec_c=array_shift($rec_c);
+
+$rec_p=$rec_prueba->mostrarTodoRegistro("cod_prueba='$cod_prueba'",0);
+$rec_p=array_shift($rec_p);
+
+$rec_b_c=$rec_banco_candidato->mostrarTodoRegistro("cod_empresa='".$cod_empresa."' and cod_recluta='$cod_recluta' and cod_banco='$cod_banco' and cedula='$cedula'",0,"nro");	
+
+if(count($rec_b_c)>0){
+	$mensaje[]="Usted Ya se Realizó esta Prueba";	
+	$sw=1;
+}else{
+	$mensaje[]="Evaluación Registrada Correctamente";	
+	$sw=0;
+}
+if(count($r)>0){
+	foreach($r as $nro=>$respuesta){
+		$rec_b=$rec_banco_preguntas->mostrarTodoRegistro("cod_banco='".$cod_banco."' and nro='$nro'",0,"nro");	
+		$rec_b=array_shift($rec_b);
+		$correcta=$rec_b['correcta'];
+		if($correcta==$respuesta){
+			$escorrecta="S";
+		}else{
+			$escorrecta="N";
+		}
+		$valores=array("cod_empresa"=>"'$cod_empresa'",
+						"cod_recluta"=>"'$cod_recluta'",
+						"cod_prueba"=>"'$cod_prueba'",
+						"cod_banco"=>"'$cod_banco'",
+						"nro"=>"'$nro'",
+						"respuesta"=>"'$respuesta'",
+						"escorrecta"=>"'$escorrecta'",
+						"cedula"=>"'$cedula'",
+		
+		);
+		$rec_b_c=$rec_banco_candidato->mostrarTodoRegistro("cod_empresa='".$cod_empresa."' and cod_recluta='$cod_recluta' and cod_banco='$cod_banco'",0,"nro");	
+		$rec_b_c=array_shift($rec_b_c);
+		if($sw==0){
+			$rec_banco_candidato->insertarRegistro($valores,0);
+			
+		}
+		
+		//print_r($rec_b_c);
+		
+		//$rec_banco_candidato->insertarRegistro($valores,0);
+		/*echo "<pre>";
+		print_r($valores);
+		echo "</pre>";*/
+	}
+	if($sw==0){
+		$_SESSION['pruebas']=$pruebas;
+	}
+}else{
+		
+}
+
+if(count($_SESSION['pruebas'])>0){
+	$Archivo="index.php";	
+	$TextoBoton="Continuar con la Prueba";
+}
+else{
+	$Archivo="terminar.php";
+	$TextoBoton="Terminar la Prueba";
+}
+
+/*
+echo "<pre>";
+print_r($_SESSION);
+print_r($_POST);
+print_r($pruebaactual);
+print_r($_SESSION);
+echo $k;
+echo $v;
+echo "</pre>";
+*/
+
+
+$titulo="Prueba de ".$rec_p['descripcion'];
+
+include_once("../cabecerahtml.php");
+include_once("../cabecera.php");
+?>
+<table class="table table-bordered">
+<thead><tr>
+<th>Cédula</th><th>Datos Personales</th>
+</tr></thead>
+<tr>
+<td><?php echo $cedula?></td>
+<td><?php echo $rec_c['paterno']." ".$rec_c['materno']." ".$rec_c['nombre']?></td>
+</tr>
+</table>
+
+<form action="evaluar.php" method="post">
+<input type="hidden" name="cod_prueba" value="<?php echo $cod_prueba?>">
+<input type="hidden" name="cod_banco" value="<?php echo $cod_banco?>">
+<?php /*echo "<pre>";
+print_r($_SESSION);
+print_r($_POST);
+echo "</pre>";*/
+?>
+<div class="col-sm-12">
+	<div class="widget-box">
+    	<div class="widget-header widget-header-flat widget-header-small">
+        <h5>Mensaje</h5></div>
+        <div class="widget-body">
+        	<div class="widget-main">
+            		<h3><strong><?php echo $mensaje[0]?></strong></h3>
+            
+					<a class="btn btn-success btn-xm" href="<?php echo $Archivo?>"><?php echo $TextoBoton?></a>
+			
+			</div>
+        </div>
+    </div>
+</div>
+
+	 
+<?php
+include_once("../pie.php");
+?>
---
+++ pruebas/prueba/selectiva.php
@@ -0,0 +1,144 @@
+<?php
+include_once("../login/check.php");
+$folder="../../";
+include_once("../../estructurabd/rec_prueba.php");
+$rec_prueba=new rec_prueba;
+
+include_once("../../estructurabd/rec_candidato.php");
+$rec_candidato=new rec_candidato;
+
+include_once("../../estructurabd/rec_banco_preguntas.php");
+$rec_banco_preguntas=new rec_banco_preguntas;
+
+$cedula=$_SESSION['cedula'];
+$cod_planta=$_SESSION['cod_planta'];
+$cod_recluta=$_SESSION['cod_recluta'];
+$pruebas=$_SESSION['pruebas'];
+$pruebaactual=array_shift($pruebas);
+$orden_prueba=array_shift(array_keys($pruebaactual));
+$cod_prueba=array_shift($pruebaactual);
+
+$rec_c=$rec_candidato->mostrarTodoRegistro("cedula='$cedula'",0);
+$rec_c=array_shift($rec_c);
+
+$rec_p=$rec_prueba->mostrarTodoRegistro("cod_prueba='$cod_prueba'",0);
+$rec_p=array_shift($rec_p);
+$cod_banco=$rec_p['cod_banco'];
+$rec_b=$rec_banco_preguntas->mostrarTodoRegistro("cod_banco='".$cod_banco."'",0,"nro");
+/*
+echo "<pre>";
+print_r($_SESSION);
+print_r($_POST);
+print_r($pruebaactual);
+print_r($_SESSION);
+echo $k;
+echo $v;
+echo "</pre>";
+*/
+$titulo="Prueba de ".$rec_p['descripcion'];
+
+include_once("../cabecerahtml.php");
+?>
+<script language="javascript" type="text/javascript" src="../../js/core/plugins/jquery.countdowntimer.min.js"></script>
+<?php
+include_once("../cabecera.php");
+?>
+<table class="table table-bordered">
+<thead><tr>
+<th>Cédula</th><th>Datos Personales</th>
+</tr></thead>
+<tr>
+<td><?php echo $cedula?></td>
+<td><?php echo $rec_c['paterno']." ".$rec_c['materno']." ".$rec_c['nombre']?></td>
+</tr>
+</table>
+<style type="text/css">
+.cuadrotiempo{
+	width:100px;
+	position:fixed;
+	top:50px;	
+	right:50px;
+	z-index:1500;
+}
+.cuadrotiempo:hover{
+	opacity:0.5;	
+}
+.cuadrotiempo .widget-main{
+	text-align:right;	
+}
+.cuadrotiempo h3{
+	margin:0px;	
+}
+</style>
+<script language="javascript" type="text/javascript">
+$(function(){
+	$("#tiempo").countdowntimer({
+		minutes : <?php echo $rec_p['tiempo']?>,
+		seconds:0,
+        size : "lg",
+		timeUp : evaluar
+	});
+	function evaluar(){
+		$("#formulario").submit();
+	}
+});
+</script>
+<div class="cuadrotiempo">
+
+        <div class="widget-box">
+        <div class="widget-header widget-header-flat widget-header-small">
+            <h5>Tiempo</h5></div>
+            <div class="widget-body">
+                <div class="widget-main resaltar">
+                <h3><strong><span id="tiempo"><?php echo $rec_p['tiempo']?></span> </strong> </h3>
+                </div>
+            </div>
+        </div>
+
+</div>
+<form action="evaluarselectiva.php" method="post" id="formulario">
+<input type="hidden" name="cod_prueba" value="<?php echo $cod_prueba?>">
+<input type="hidden" name="cod_banco" value="<?php echo $cod_banco?>">
+
+<?php foreach($rec_b as $rb){?>
+<div class="col-sm-12">
+	<div class="widget-box">
+    	<div class="widget-header widget-header-flat widget-header-small">
+        <h5><?php echo $rb['nro']?> - <?php echo $rb['pregunta']?></h5></div>
+        <div class="widget-body">
+        	<div class="widget-main">
+            <table class="table table-bordered">
+            	<tr>
+                	<?php for($i=1;$i<=5;$i++){?>
+                	<td class="centrar" width="250">
+                    	<?php 
+						if($rb['opcion'.$i]!=""){
+							?>
+                            <label>
+                            <?php
+							echo $rb['opcion'.$i];
+							?>
+                            <br>
+                            <input type="radio" name="r[<?php echo $rb['nro']?>]" value="<?php echo $i?>" >
+                            </label>
+                            <?php
+						}
+							?>	
+                    </td>
+                    <?php }?>
+                </tr>
+            </table>
+            
+					
+			
+			</div>
+        </div>
+    </div>
+</div>
+<?php }?>
+	 <input type="submit" value="Grabar" class="btn btn-danger btn-xm">
+</form>
+<?php
+include_once("../pie.php");
+
+?>
