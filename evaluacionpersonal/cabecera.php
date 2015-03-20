3e14d2164cf7d595e65df33c5e318ef8be3145f8
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-03-19 22:55:38 -0400

    Agregado Menu

--- evaluacionpersonal/cabecera.php
+++ evaluacionpersonal/cabecera.php
@@ -327,7 +327,7 @@
                         </li>
                         <li class="<?php echo $rmenu=="/"?'active':''?>"><a href="<?php echo $folder;?>evaluacionpersonal/index.php"><i class="arrow icon-angle-down"></i><span> <?php echo 'Grupos de Evaluación'?></span></a>
                         </li>
-                        <li class="<?php echo $rmenu=="/"?'active':''?>"><a href="<?php echo $folder;?>evaluacionpersonal/index.php"><i class="arrow icon-angle-down"></i><span> <?php echo 'Competencias'?></span></a>
+                        <li class="<?php echo $rmenu=="/"?'active':''?>"><a href="<?php echo $folder;?>evaluacionpersonal/competencias/index.php?c=competencia"><i class="arrow icon-angle-down"></i><span> <?php echo 'Competencias'?></span></a>
                         </li>
                         <li class="<?php echo $rmenu=="/"?'active':''?>"><a href="<?php echo $folder;?>evaluacionpersonal/index.php"><i class="arrow icon-angle-down"></i><span> <?php echo 'Reportes'?></span></a>
                         </li>
