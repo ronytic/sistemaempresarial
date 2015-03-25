a179d2da0f89578aa986a4f5a40988973a73e444
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-03-25 07:27:34 -0400

    BD Concentracion

---
+++ estructurabd/rec_banco_concentracion.php
@@ -0,0 +1,40 @@
+<?php
+include_once(dirname(__FILE__).DIRECTORY_SEPARATOR."../class/bd.php");
+class rec_banco_concentracion extends bd{
+	var $campos_tabla=array(
+		"cod_concentracion"=>array("Tipo"=>"int","Tamano"=>"11","NoNulo"=>true,"Primaria"=>true,"AI"=>true),
+		"campo1"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo2"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo3"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo4"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo5"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo6"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo7"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo8"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo9"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo10"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo11"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo12"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo13"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo14"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo15"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo16"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo17"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo18"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo19"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo20"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo21"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo22"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo23"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo24"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo25"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo26"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo27"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo28"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo29"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo30"=>array("Tipo"=>"int","Tamano"=>"11"),
+        "campo31"=>array("Tipo"=>"int","Tamano"=>"11"),
+		"respuesta"=>array("Tipo"=>"varchar","Tamano"=>"50"),
+	);	
+}
+?>
