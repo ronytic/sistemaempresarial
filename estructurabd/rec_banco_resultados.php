7b9ad9a85b8001341a7e67a9922e54babea7816a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-04-12 17:04:22 -0400

    Banco resultados

---
+++ estructurabd/rec_banco_resultados.php
@@ -0,0 +1,15 @@
+<?php
+include_once(dirname(__FILE__).DIRECTORY_SEPARATOR."../class/bd.php");
+class rec_banco_candidato extends bd{
+	var $campos_tabla=array(
+		"codigo_banco_resultados"=>array("Tipo"=>"int","Tamano"=>"11","NoNulo"=>true,"Primaria"=>true,"AI"=>true),
+		"cod_empresa"=>array("Tipo"=>"varchar","Tamano"=>"3"),
+		"cod_recluta"=>array("Tipo"=>"int","Tamano"=>"11"),
+		"cod_prueba"=>array("Tipo"=>"varchar","Tamano"=>"3"),
+		"correstas"=>array("Tipo"=>"double","Tamano"=>""),
+		"incorrectas"=>array("Tipo"=>"double","Tamano"=>""),
+		"total"=>array("Tipo"=>"double","Tamano"=>""),
+		"cedula"=>array("Tipo"=>"varchar","Tamano"=>"12"),
+	);	
+}
+?>
